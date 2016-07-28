47dbc80d89c480859f7b13c3f2bdd367343f4f7d
Sergey Atroshchenko <sergey.atroshchenko@yandex.ru>
2016-07-28 17:25:52 +0300

    add request runtime permissions

--- app/src/main/java/com/example/user/yamblz_find_number/MainActivity.java
+++ app/src/main/java/com/example/user/yamblz_find_number/MainActivity.java
@@ -1,23 +1,46 @@
 package com.example.user.yamblz_find_number;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 public class MainActivity extends Activity {
     private static final String LOG_TAG = "LOG";
+    private static int OVERLAY_PERMISSION_REQ_CODE = 1234;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         someMethod();
+        if (!checkPermission()) {
+            requestPermission();
+        }
     }
 
-    private static int OVERLAY_PERMISSION_REQ_CODE = 1234;
+    private boolean checkPermission() {
+        return hasPermission(Manifest.permission.READ_PHONE_STATE);
+    }
+
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+    }
+
+    private boolean hasPermission(@NonNull String... permissions) {
+        for (String p : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+        return true;
+    }
 
     private void someMethod() {
         if (!Settings.canDrawOverlays(this)) {
