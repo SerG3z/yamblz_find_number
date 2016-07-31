bae2c4e26e6b6f91fc8220bd2868c00016eb48f5
Sergey Atroshchenko <sergey.atroshchenko@yandex.ru>
2016-08-01 01:14:58 +0300

    remove unnecessary flags

--- app/src/main/java/com/example/user/yamblz_find_number/CallReceiver.java
+++ app/src/main/java/com/example/user/yamblz_find_number/CallReceiver.java
@@ -50,10 +50,7 @@ public class CallReceiver extends BroadcastReceiver {
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                        | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                        | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR, 0,
                 PixelFormat.TRANSLUCENT);
         params.gravity = Gravity.TOP;
 
